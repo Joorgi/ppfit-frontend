96fbf23e18f14a6c09ccdc8b017bb4a37ac91210
Jose Luis <jortiz@koibox.cloud>
2026-03-15 00:22:29 +0100

    feat: Agregar encabezado y pie de página a la aplicación
    Se implementan componentes de encabezado y pie de página para mejorar la navegación y la presentación de la aplicación.

---
+++ app/components/header.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import type {NavigationMenuItem} from '@nuxt/ui'
+
+const route = useRoute()
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Docs',
+    to: '/docs/getting-started',
+    active: route.path.startsWith('/docs/getting-started')
+  },
+  {
+    label: 'Components',
+    to: '/docs/components',
+    active: route.path.startsWith('/docs/components')
+  },
+  {
+    label: 'Figma',
+    to: 'https://go.nuxt.com/figma-ui',
+    target: '_blank'
+  },
+  {
+    label: 'Releases',
+    to: 'https://github.com/nuxt/ui/releases',
+    target: '_blank'
+  }
+])
+</script>
+
+<template>
+  <UHeader>
+    <template #title>
+      <NuxtLink to="/" class="flex items-center gap-2">
+        <NuxtImg
+            src="/logo.png"
+            alt="Logo ppfit"
+            width="60"
+            height="60"
+        />
+        <span class="font-black text-xl tracking-tight text-primary-500">ppfit</span>
+      </NuxtLink>
+    </template>
+
+    <UNavigationMenu :items="items"/>
+
+    <template #right>
+      <UColorModeButton/>
+
+      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
+        <UButton
+            color="neutral"
+            variant="ghost"
+            to="https://github.com/nuxt/ui"
+            target="_blank"
+            icon="i-simple-icons-github"
+            aria-label="GitHub"
+        />
+      </UTooltip>
+    </template>
+  </UHeader>
+</template>
