5ea9175a901c707ac01cd4922d9cdb37f115b1f0
Jose Luis <jortiz@koibox.cloud>
2026-03-14 23:04:43 +0100

    feat: Agregar función useApiFetch para solicitudes API
    Implementa un hook para realizar solicitudes a la API con manejo de autenticación y errores.

---
+++ app/composables/useApiFetch.ts
@@ -0,0 +1,29 @@
+import type {UseFetchOptions} from '#app'
+
+export function useApiFetch<T = any>(
+  request: string | (() => string),
+  opts?: UseFetchOptions<T>
+) {
+  const config = useRuntimeConfig()
+  const token = useCookie<string | null>('auth_token')
+
+  const options: UseFetchOptions<T> = {
+    baseURL: config.public.apiBase,
+    ...opts,
+    headers: {
+      Authorization: token.value ? `Bearer ${token.value}` : '',
+      ...opts?.headers,
+    },
+    onResponseError(context) {
+      if (context.response.status === 401) {
+        console.warn('El token ha caducado o es inválido.')
+      }
+    },
+    onResponse(context) {
+      if (context.response.status === 200) {
+        console.log('Respuesta exitosa')
+      }
+    },
+  }
+  return useFetch<T>(request, options as any)
+}
