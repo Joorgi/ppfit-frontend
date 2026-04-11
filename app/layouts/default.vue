b745452e91129d41dce2ee3e5701a6c3b30d3d5f
Jose Luis <joselustudios@gmail.com>
2026-04-11 16:51:12 +0200

    style: Mejorar estructura del layout en default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-  <Header />
-  <slot />
-  <Footer />
+  <div class="min-h-screen flex flex-col bg-gray-50 dark:bg-dark transition-colors duration-300">
+    <Header />
+
+    <main class="flex-1">
+      <slot />
+    </main>
+
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
