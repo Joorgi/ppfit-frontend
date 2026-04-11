a6a0b7eb51856a35a3856b43d3536fa8e0fbee2e
Jose Luis <joselustudios@gmail.com>
2026-04-11 18:45:16 +0200

    feat: Validar rutas existentes en middleware de autenticación

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,9 +1,16 @@
 import { useAuthStore } from '~/stores/auth'
 
 export default defineNuxtRouteMiddleware((to, _) => {
-  const authStore = useAuthStore() // Tu store de Pinia
+  const authStore = useAuthStore()
+  const router = useRouter()
 
   const publicRoutes = ['/', '/login', '/register']
+
+  const routeExists = router.getRoutes().some(route => route.path === to.path)
+
+  if (!routeExists) {
+    return
+  }
 
   if (!publicRoutes.includes(to.path) && !authStore.isAuthenticated) {
     return navigateTo('/login')
