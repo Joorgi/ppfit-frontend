894ede272087ddc85985396a9033bd029232af2d
Jose Luis <joselustudios@gmail.com>
2026-04-13 19:47:25 +0200

    refactor: Mejorar uso de API con opciones personalizadas

--- app/composables/useAPI.ts
+++ app/composables/useAPI.ts
@@ -1,11 +1,39 @@
-export const useAPI = createUseFetch({
-  baseURL: useRuntimeConfig().public.apiBase,
-  headers: {
-    Authorization: useCookie<string | null>('auth_token').value ? `Bearer ${useCookie<string | null>('auth_token').value}` : '',
-  },
-  onResponseError(context) {
-    if (context.response.status === 401) {
-      console.warn('El token ha caducado o es inválido.')
-    }
-  },
-})
+// const config = useRuntimeConfig()
+// const token = useCookie<string | null>('auth_token')
+//
+// export const useAPI = createUseFetch({
+//   baseURL: config.public.apiBase,
+//   headers: {
+//     Authorization: token.value ? `Bearer ${token.value}` : '',
+//   },
+// })
+
+import type { UseFetchOptions } from '#app'
+
+export function useAPI<T = any>(
+  request: string | (() => string),
+  opts?: UseFetchOptions<T>,
+) {
+  const config = useRuntimeConfig()
+  const token = useCookie<string | null>('auth_token')
+
+  const options: UseFetchOptions<T> = {
+    baseURL: config.public.apiBase,
+    ...opts,
+    headers: {
+      Authorization: token.value ? `Bearer ${token.value}` : '',
+      ...opts?.headers,
+    },
+    onResponseError(context) {
+      if (context.response.status === 401) {
+        console.warn('El token ha caducado o es inválido.')
+      }
+    },
+    onResponse(context) {
+      if (context.response.status === 200) {
+        console.log('Respuesta exitosa')
+      }
+    },
+  }
+  return useFetch<T>(request, options as any)
+}
