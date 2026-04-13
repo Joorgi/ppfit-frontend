a5366dc045f4cff24eb7bf24447513044dd155aa
Jose Luis <joselustudios@gmail.com>
2026-04-13 19:12:33 +0200

    feat: Agregar configuración de toaster y nuevos colores

--- app/app.vue
+++ app/app.vue
@@ -2,10 +2,18 @@
 import * as locales from '@nuxt/ui/locale'
 
 const { locale } = useI18n()
+const toaster: {
+  duration: number
+  position: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
+  progress: boolean
+} = { duration: 2500, position: 'top-right', progress: false }
 </script>
 
 <template>
-  <UApp :locale="locales[locale]">
+  <UApp
+    :locale="locales[locale]"
+    :toaster="toaster"
+  >
     <UMain>
       <NuxtLayout>
         <NuxtPage />
